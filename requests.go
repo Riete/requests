bace45fb63be24cdef23747dfd8add2b44f89f46
Riete <loli_520@126.com>
2020-05-27 20:29:02 +0800

    代码优化

--- requests.go
+++ requests.go
@@ -10,119 +10,121 @@ import (
 )
 
 const (
-	ContentTypeJson string = "application/json;charset=utf-8"
-	ContentTypeForm string = "application/x-www-form-urlencoded"
+	contentTypeJson string = "application/json;charset=utf-8"
+	contentTypeForm string = "application/x-www-form-urlencoded"
+	httpGet         string = "GET"
+	httpPost        string = "POST"
 )
 
-type Request struct {
-	request  *http.Request
-	client   *http.Client
-	response *Response
+type request struct {
+	httpreq *http.Request
+	client  *http.Client
+	httprsp *response
 }
 
-type Response struct {
+type response struct {
 	resp *http.Response
 }
 
-func NewRequest() *Request {
-	r := &Request{}
+func newRequest() *request {
+	r := &request{}
 	r.client = &http.Client{}
-	r.request, _ = http.NewRequest("", "", nil)
-	r.response = &Response{}
+	r.httpreq, _ = http.NewRequest("", "", nil)
+	r.httprsp = &response{}
 	return r
 }
 
-func (r *Request) do() (*Response, error) {
-	resp, err := r.client.Do(r.request)
-	r.response.resp = resp
-	return r.response, err
+func (r *request) do() (*response, error) {
+	resp, err := r.client.Do(r.httpreq)
+	r.httprsp.resp = resp
+	return r.httprsp, err
 }
 
-func (r *Request) Get(originUrl string) (*Response, error) {
-	r.request.Method = GET
-	r.request.URL = parseUrl(originUrl)
+func (r *request) get(originUrl string) (*response, error) {
+	r.httpreq.Method = httpGet
+	r.httpreq.URL = parseUrl(originUrl)
 	return r.do()
 }
 
-func (r *Request) GetWithParams(originUrl string, params map[string]string) (*Response, error) {
-	r.request.Method = GET
-	r.request.URL = parseUrl(originUrl)
+func (r *request) getWithParams(originUrl string, params map[string]string) (*response, error) {
+	r.httpreq.Method = httpGet
+	r.httpreq.URL = parseUrl(originUrl)
 	sendUrl := parseUrl(originUrl)
 	p := url.Values{}
 	for k, v := range params {
 		p.Add(k, v)
 	}
 	sendUrl.RawQuery = p.Encode()
-	r.request.URL = sendUrl
+	r.httpreq.URL = sendUrl
 	return r.do()
 }
 
-func (r *Request) Post(originUrl string) (*Response, error) {
-	r.request.Method = POST
-	r.request.URL = parseUrl(originUrl)
+func (r *request) post(originUrl string) (*response, error) {
+	r.httpreq.Method = httpPost
+	r.httpreq.URL = parseUrl(originUrl)
 	return r.do()
 }
 
-func (r *Request) PostJson(originUrl string, data map[string]interface{}) (*Response, error) {
-	r.request.Method = POST
-	r.request.URL = parseUrl(originUrl)
+func (r *request) postJson(originUrl string, data map[string]interface{}) (*response, error) {
+	r.httpreq.Method = httpPost
+	r.httpreq.URL = parseUrl(originUrl)
 	jsonStr, _ := json.Marshal(data)
-	r.request.Header.Set("Content-Type", ContentTypeJson)
-	r.request.Body = ioutil.NopCloser(bytes.NewBuffer(jsonStr))
+	r.httpreq.Header.Set("Content-Type", contentTypeJson)
+	r.httpreq.Body = ioutil.NopCloser(bytes.NewBuffer(jsonStr))
 	return r.do()
 }
 
-func (r *Request) PostForm(originUrl string, data map[string]string) (*Response, error) {
-	r.request.Method = POST
-	r.request.URL = parseUrl(originUrl)
-	r.request.Header.Set("Content-Type", ContentTypeForm)
+func (r *request) postForm(originUrl string, data map[string]string) (*response, error) {
+	r.httpreq.Method = httpPost
+	r.httpreq.URL = parseUrl(originUrl)
+	r.httpreq.Header.Set("Content-Type", contentTypeForm)
 	formData := url.Values{}
 	for k, v := range data {
 		formData.Add(k, v)
 	}
-	r.request.Body = ioutil.NopCloser(strings.NewReader(formData.Encode()))
+	r.httpreq.Body = ioutil.NopCloser(strings.NewReader(formData.Encode()))
 	return r.do()
 }
 
-func (r *Response) Content() string {
+func (r *response) Content() string {
 	defer r.resp.Body.Close()
 	body, _ := ioutil.ReadAll(r.resp.Body)
 	return string(body)
 }
 
-func (r *Response) StatusCode() int {
+func (r *response) StatusCode() int {
 	return r.resp.StatusCode
 }
 
-func (r *Response) Status() string {
+func (r *response) Status() string {
 	return r.resp.Status
 }
 
-func (r *Response) Cookies() []*http.Cookie {
+func (r *response) Cookies() []*http.Cookie {
 	return r.resp.Cookies()
 }
 
-func Get(originUrl string) (*Response, error) {
-	r := NewRequest()
-	return r.Get(originUrl)
+func Get(originUrl string) (*response, error) {
+	r := newRequest()
+	return r.get(originUrl)
 }
 
-func GetWithParams(originUrl string, params map[string]string) (*Response, error) {
-	r := NewRequest()
-	return r.GetWithParams(originUrl, params)
+func GetWithParams(originUrl string, params map[string]string) (*response, error) {
+	r := newRequest()
+	return r.getWithParams(originUrl, params)
 }
 
-func Post(originUrl string) (*Response, error) {
-	r := NewRequest()
-	return r.Post(originUrl)
+func Post(originUrl string) (*response, error) {
+	r := newRequest()
+	return r.post(originUrl)
 }
 
-func PostJson(originUrl string, data map[string]interface{}) (*Response, error) {
-	r := NewRequest()
-	return r.PostJson(originUrl, data)
+func PostJson(originUrl string, data map[string]interface{}) (*response, error) {
+	r := newRequest()
+	return r.postJson(originUrl, data)
 }
 
-func PostForm(originUrl string, data map[string]string) (*Response, error) {
-	r := NewRequest()
-	return r.PostForm(originUrl, data)
+func PostForm(originUrl string, data map[string]string) (*response, error) {
+	r := newRequest()
+	return r.postForm(originUrl, data)
 }
--- session.go
+++ session.go
@@ -15,21 +15,16 @@ import (
 	"time"
 )
 
-const (
-	GET  string = "GET"
-	POST string = "POST"
-)
-
-type Session struct {
+type session struct {
 	cookies []*http.Cookie
-	Request
+	request
 }
 
-func NewSession() *Session {
-	s := &Session{}
+func NewSession() *session {
+	s := &session{}
 	s.client = &http.Client{}
-	s.response = &Response{}
-	s.request, _ = http.NewRequest("", "", nil)
+	s.httprsp = &response{}
+	s.httpreq, _ = http.NewRequest("", "", nil)
 	jar, _ := cookiejar.New(nil)
 	s.client.Jar = jar
 	return s
@@ -43,51 +38,51 @@ func parseUrl(originUrl string) *url.URL {
 	return sendUrl
 }
 
-func (s *Session) SetBasicAuth(username, password string) {
-	s.request.SetBasicAuth(username, password)
+func (s *session) SetBasicAuth(username, password string) {
+	s.httpreq.SetBasicAuth(username, password)
 }
 
-func (s *Session) SetBearTokenAuth(token string) {
-	s.request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+func (s *session) SetBearTokenAuth(token string) {
+	s.httpreq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 }
 
-func (s *Session) JsonAuth(originUrl string, data map[string]interface{}) (*Response, error) {
+func (s *session) JsonAuth(originUrl string, data map[string]interface{}) (*response, error) {
 	resp, err := PostJson(originUrl, data)
 	if err != nil {
 		return nil, err
 	}
-	s.response = resp
-	s.cookies = s.response.Cookies()
-	return s.response, nil
+	s.httprsp = resp
+	s.cookies = s.httprsp.Cookies()
+	return s.httprsp, nil
 }
 
-func (s *Session) FormAuth(originUrl string, data map[string]string) (*Response, error) {
+func (s *session) FormAuth(originUrl string, data map[string]string) (*response, error) {
 	resp, err := PostForm(originUrl, data)
 	if err != nil {
 		return nil, err
 	}
-	s.response = resp
-	s.cookies = s.response.Cookies()
-	return s.response, nil
+	s.httprsp = resp
+	s.cookies = s.httprsp.Cookies()
+	return s.httprsp, nil
 }
 
-func (s *Session) setCookies(originUrl string) {
+func (s *session) setCookies(originUrl string) {
 	sendUrl := parseUrl(originUrl)
 	s.client.Jar.SetCookies(sendUrl, s.cookies)
-	s.request.URL = sendUrl
+	s.httpreq.URL = sendUrl
 }
 
-func (s *Session) do() (*Response, error) {
-	resp, err := s.client.Do(s.request)
-	s.response.resp = resp
-	return s.response, err
+func (s *session) do() (*response, error) {
+	resp, err := s.client.Do(s.httpreq)
+	s.httprsp.resp = resp
+	return s.httprsp, err
 }
 
-func (s *Session) SetTimeout(t time.Duration) {
+func (s *session) SetTimeout(t time.Duration) {
 	s.client.Timeout = t * time.Second
 }
 
-func (s *Session) SkipTLSVerify() {
+func (s *session) SkipTLSVerify() {
 	tr := &http.Transport{
 		Proxy: http.ProxyFromEnvironment,
 		DialContext: (&net.Dialer{
@@ -105,7 +100,7 @@ func (s *Session) SkipTLSVerify() {
 	s.client.Transport = tr
 }
 
-func (s *Session) SetProxy(proxy map[string]string) {
+func (s *session) SetProxy(proxy map[string]string) {
 	for k, v := range proxy {
 		err := os.Setenv(k, v)
 		if err != nil {
@@ -114,48 +109,48 @@ func (s *Session) SetProxy(proxy map[string]string) {
 	}
 }
 
-func (s *Session) Get(originUrl string) (*Response, error) {
+func (s *session) Get(originUrl string) (*response, error) {
 	s.setCookies(originUrl)
-	s.request.Method = GET
+	s.httpreq.Method = httpGet
 	return s.do()
 }
 
-func (s *Session) GetWithParams(originUrl string, params map[string]string) (*Response, error) {
+func (s *session) GetWithParams(originUrl string, params map[string]string) (*response, error) {
 	s.setCookies(originUrl)
-	s.request.Method = GET
+	s.httpreq.Method = httpGet
 	sendUrl := parseUrl(originUrl)
 	p := url.Values{}
 	for k, v := range params {
 		p.Add(k, v)
 	}
 	sendUrl.RawQuery = p.Encode()
-	s.request.URL = sendUrl
+	s.httpreq.URL = sendUrl
 	return s.do()
 }
 
-func (s *Session) Post(originUrl string) (*Response, error) {
+func (s *session) Post(originUrl string) (*response, error) {
 	s.setCookies(originUrl)
-	s.request.Method = POST
+	s.httpreq.Method = httpPost
 	return s.do()
 }
 
-func (s *Session) PostJson(originUrl string, data map[string]interface{}) (*Response, error) {
+func (s *session) PostJson(originUrl string, data map[string]interface{}) (*response, error) {
 	s.setCookies(originUrl)
-	s.request.Method = POST
+	s.httpreq.Method = httpPost
 	jsonStr, _ := json.Marshal(data)
-	s.request.Header.Set("Content-Type", ContentTypeJson)
-	s.request.Body = ioutil.NopCloser(bytes.NewBuffer(jsonStr))
+	s.httpreq.Header.Set("Content-Type", contentTypeJson)
+	s.httpreq.Body = ioutil.NopCloser(bytes.NewBuffer(jsonStr))
 	return s.do()
 }
 
-func (s *Session) PostForm(originUrl string, data map[string]string) (*Response, error) {
+func (s *session) PostForm(originUrl string, data map[string]string) (*response, error) {
 	s.setCookies(originUrl)
-	s.request.Method = POST
-	s.request.Header.Set("Content-Type", ContentTypeForm)
+	s.httpreq.Method = httpPost
+	s.httpreq.Header.Set("Content-Type", contentTypeForm)
 	formData := url.Values{}
 	for k, v := range data {
 		formData.Add(k, v)
 	}
-	s.request.Body = ioutil.NopCloser(strings.NewReader(formData.Encode()))
+	s.httpreq.Body = ioutil.NopCloser(strings.NewReader(formData.Encode()))
 	return s.do()
 }
